729b54eaff3ac23653f1aaf7d7c37be48c563ae0
braybowler <braybowler1995@gmail.com>
2025-12-09 08:46:55 -0500

    api healthcheck route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/up', function () {
+    return 'Wedge Matrix API is up and ready for requests.';
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
