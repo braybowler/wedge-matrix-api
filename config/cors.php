6581ad47e0ade644bb51d6b333de227ade8fecd5
braybowler <braybowler1995@gmail.com>
2026-02-16 09:59:51 -0500

    update allowed origins

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,6 @@ return [
     'allowed_origins' => [
         'https://wedgematrix.ca',
         'https://www.wedgematrix.ca',
-
         'http://localhost:5173',
         'http://127.0.0.1:5173',
     ],
