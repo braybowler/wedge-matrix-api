46e8147f72a4761717498b72e317a7be403aa00c
braybowler <braybowler1995@gmail.com>
2026-03-01 16:50:05 -0500

    introduce emails for account creation and deletion

--- resources/views/mail/account-deletion.blade.php
+++ resources/views/mail/account-deletion.blade.php
@@ -6,5 +6,5 @@ Your Wedge Matrix account and all associated data have been permanently deleted.
 If you did not request this, please contact us immediately.
 
 Thanks,<br>
-{{ config('app.name') }}
+The {{ config('app.name') }} Team
 </x-mail::message>
